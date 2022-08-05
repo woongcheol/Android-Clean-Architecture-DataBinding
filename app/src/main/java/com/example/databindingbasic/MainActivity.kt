f285b11d702d98e8b5cc7ecf8d680b2a25a5ae8c
Woongcheol Shin <1994_100@naver.com>
2022-08-05 15:18:17 +0900

    [FEAT] Text Change Button 추가 - Data Binding를 통한 데이터 변경 기능

--- app/src/main/java/com/example/databindingbasic/MainActivity.kt
+++ app/src/main/java/com/example/databindingbasic/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.databindingbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingbasic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var text = "No Change Text"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        binding.activity = this
+        binding.btnChange.setOnClickListener {
+            text = "Change Text"
+            binding.invalidateAll()
+        }
     }
 }
